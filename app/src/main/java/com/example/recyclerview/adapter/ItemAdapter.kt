bafa9cb988e3caac46be7e3712b4706189d2f59c
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 09:28:18 +0600

    ItemAdapter.kt bug fixed, created list_item.xml

--- app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/recyclerview/adapter/ItemAdapter.kt
@@ -20,11 +20,14 @@ class ItemAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
-        
+            .inflate(R.layout.list_item, parent, false)
+
+        return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataset[position]
+        
     }
 
     override fun getItemCount(): Int {
