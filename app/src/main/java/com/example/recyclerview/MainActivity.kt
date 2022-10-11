b7ad4f08a86e862a9dd6a701d60870d434b7e7cd
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 09:23:37 +0600

    Created model, Affirmation.kt

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,15 +2,10 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.example.recyclerview.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.textview)
-        textView.text = Datasource().loadAffirmations().size.toString()
-
     }
 }
