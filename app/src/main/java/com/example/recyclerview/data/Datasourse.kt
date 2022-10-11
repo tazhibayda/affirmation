b0bd73eb7dde10ad947125efe03c4e4923154d78
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 09:26:49 +0600

    Created adapter, ItemAdapter.kt

--- app/src/main/java/com/example/recyclerview/data/Datasourse.kt
+++ app/src/main/java/com/example/recyclerview/data/Datasourse.kt
@@ -4,6 +4,20 @@ import com.example.recyclerview.R
 import com.example.recyclerview.models.Affirmation
 
 
-class Datasourse {
+class Datasource {
 
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
 }
